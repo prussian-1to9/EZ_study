d2088f60583bb779331265d868e059ad9318a849
prussian1to9 <prussian.3to3@gmail.com>
2024-08-14 17:14:13 +0900

    [FEAT] codeit source avatar proj. : js template

--- codeit_avatar/script.js
+++ codeit_avatar/script.js
@@ -1,1 +1,36 @@
-/* 여기에 코드를 입력하세요. */
+/* =============== [ selectors ] =============== */
+// 아바타 영역
+const avatarTone = document.querySelector('#avatar-tone');
+
+// 메뉴 영역
+const avatarSelectorTone = document.querySelector('#avatar-selector-tone');
+
+/* =============== [ 함수 선언 ] =============== */
+// 메뉴 변경
+const updateAvatarMenu = () => {
+    // 클릭한 항목에 해당하는 아바타 셀렉터 보여 주기
+    // 클릭한 항목에 해당하는 아바타 메뉴 항목 강조
+}
+
+
+
+const updateAvatar = () => {
+    // 클릭 항목에 해당하는 이미지로 아바타 변경
+}
+
+// TEST : 피부톤 변경
+const handleAvatarSelectorClick = (e) => {
+    // 사용자가 아바타 셀렉터 항목을 클릭했을 때 실행할 이벤트 핸들러
+    // updateAvatar() 함수를 실행
+    const selectedTone = e.target.dataset.avatarValue;
+    if (!selectedTone) return;
+    avatarTone.setAttribute('src', `./images/avatar/${selectedTone}.svg`);
+}
+
+const handleSaveclick = () => {
+    // 사용자가 저장하기 버튼을 클릭했을 때 실행할 이벤트 핸들러
+    // 이미지를 저장하는 코드 실행
+}
+
+/* =============== [ 이벤트 삽입 ] =============== */
+avatarSelectorTone.addEventListener('click', handleAvatarSelectorClick); // 피부톤
