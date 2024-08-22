a903cbedbdb473ee0e73b3f23b69a1c66755ee02
prussian1to9 <prussian.3to3@gmail.com>
2024-08-22 09:44:23 +0900

    [FEAT] codeit avatar proj : avatar saving feature

--- codeit-sources/interactive-javascript/avatar-project/script.js
+++ codeit-sources/interactive-javascript/avatar-project/script.js
@@ -8,14 +8,14 @@ const currentAvatar = {
     clothes: 'hoodie',
     accessories: 'acc_none',
 }
-const avatarTone = document.querySelector('#avatar-tone');
-
 // selectors
-const avatarMenu = document.querySelector('#avatar-menu');
+const avatar        = document.querySelector('#avatar');
+const avatarMenu    = document.querySelector('#avatar-menu');
 const avatarMenuItems = document.querySelectorAll('.avatar-menu-item');
 const avatarSelectors = document.querySelectorAll('.avatar-selector');
-const avatarImages = document.querySelectorAll('.avatar-image');
-const avatarSelectorTone = document.querySelector('#avatar-selector-tone');
+const avatarImages  = document.querySelectorAll('.avatar-image');
+const saveButton    = document.querySelector('#save-button');
+
 
 /* =============== [ 비즈니스 로직 ] =============== */
 // 메뉴 변경
@@ -45,13 +45,14 @@ const updateAvatar = (avatarKey, avatarValue) => {
         imageKey = 'hair';
         fileName = `${currentAvatar.hair_type}-${currentAvatar.hair_color}`;
     }
-
+    // 변경된 이미지 동기화
     for (const avatarImage of avatarImages) {
         if (avatarImage.dataset.imageKey === imageKey) {
             avatarImage.src = `./images/avatar/${fileName}.svg`;
         }
     }
 }
+
 
 /* =============== [ 핸들러 ] =============== */
 // 메뉴 변경
@@ -67,29 +68,23 @@ const handleAvatarSelectorClick = (e) => {
     if (!nextAvatarValue) return;
     updateAvatar(currentAvatarKey, nextAvatarValue);
 }
-
-
-
-
-
-
-/* TEST : 피부톤 변경
-const handleAvatarSelectorClick = (e) => {
-    // 사용자가 아바타 셀렉터 항목을 클릭했을 때 실행할 이벤트 핸들러
-    // updateAvatar() 함수를 실행
-    const selectedTone = e.target.dataset.avatarValue;
-    if (!selectedTone) return;
-    avatarTone.setAttribute('src', `./images/avatar/${selectedTone}.svg`);
-}
-*/
+// 아바타 이미지 저장
 const handleSaveclick = () => {
-    // 사용자가 저장하기 버튼을 클릭했을 때 실행할 이벤트 핸들러
-    // 이미지를 저장하는 코드 실행
+    html2canvas(avatar).then((canvas) => {
+        const link = document.createElement('a');
+        link.href = canvas.toDataURL('image/png');
+        link.download = 'avatar.png';
+        link.click();
+    });
 }
+
 
 /* =============== [ 이벤트 삽입 ] =============== */
-avatarMenu.addEventListener('click', handleAvatarMenu); // 메뉴 변경
+// 메뉴 변경
+avatarMenu.addEventListener('click', handleAvatarMenu);
+// 아바타 컴포넌트 변경
 avatarSelectors.forEach((avatarSelector) => {
     avatarSelector.addEventListener('click', handleAvatarSelectorClick);
-}); // 아바타 컴포넌트 변경
-avatarSelectorTone.addEventListener('click', handleAvatarSelectorClick); // 피부톤
+});
+// 아바타 이미지 저장
+saveButton.addEventListener('click', handleSaveclick);
