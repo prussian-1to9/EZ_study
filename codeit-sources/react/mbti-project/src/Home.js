b49329795f1d2d0cda254ba3d24d59e3b2dabe6c
prussian1to9 <prussian.3to3@gmail.com>
2024-09-25 10:34:02 +0900

    [FEAT] codeit MBTI proj. : can load infinity
    + fixed : home CSS (can include longer ids)
    + MBTI project done

--- codeit-sources/react/mbti-project/src/Home.js
+++ codeit-sources/react/mbti-project/src/Home.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import ColorSurvey from "./components/MBTISelect/ColorSurvey";
 import axios from "./lib/axios";
@@ -8,17 +8,47 @@ export default function Home() {
   const [items, setItems] = useState([]); // MBTI-color data
   const [filter, setFilter] = useState(null); // MBTI filter
 
+  const nextPageRef = useRef(null); // pagination Ref
+  const isLoadingRef = useRef(false); // prevent multiple requests
+
   const handleLoad = async (mbti) => {
     const res = await axios("/color-surveys/", {
       params: { mbti, limit: 20 },
     });
+    nextPageRef.current = res.data.next; // next request URL
     const { results } = res.data;
     setItems(results);
   };
 
+  const handleLoadNext = async () => {
+    const res = await axios(nextPageRef.current);
+    const { results, next } = res.data;
+    setItems((prev) => [...prev, ...results]);
+    nextPageRef.current = next;
+  };
+
+  /* cleanup => render => useEffect(from child nodes) */
   useEffect(() => {
     handleLoad(filter);
   }, [filter]);
+
+  useEffect(() => {
+    // when scroll to the bottom of the page
+    const handleScroll = async () => {
+      if (!nextPageRef.current || isLoadingRef.current) return;
+      isLoadingRef.current = true;
+
+      const maxScrollTop =
+        document.documentElement.offsetHeight - window.innerHeight - 100; // padding
+      const currentScrollTop = document.documentElement.scrollTop;
+
+      if (currentScrollTop >= maxScrollTop) await handleLoadNext();
+      isLoadingRef.current = false;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
 
   return (
     <div className={styles.container}>
