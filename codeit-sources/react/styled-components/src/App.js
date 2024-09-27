beb2e3ba287cc7bf5901266a594da8c31253aa79
prussian1to9 <prussian.3to3@gmail.com>
2024-09-27 10:48:21 +0900

    [FEAT] codeit styled components : lecure2(adv.) global style

--- codeit-sources/react/styled-components/src/App.js
+++ codeit-sources/react/styled-components/src/App.js
@@ -1,12 +1,28 @@
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import Container from "./components/Container";
-import SignInHeader from "./components/Header";
+import Header from "./components/Header";
 import Label from "./components/Label";
 import Input from "./components/Input";
 import Button from "./components/Button";
 import Link from "./components/Link";
 import KakaoButton from "./components/KakaoButton";
 import CodeitLogoImg from "./components/icons/codeit.png";
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-family: Pretendard, sans-serif;
+  }
+  body {
+    margin: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+      monospace;
+  }
+`;
 
 const CodeitLogo = styled.img`
   display: block;
@@ -17,19 +33,22 @@ const CodeitLogo = styled.img`
 
 function App() {
   return (
-    <Container>
-      <SignInHeader>
-        <CodeitLogo src={CodeitLogoImg} alt="Codeit" />
-        Have you Signed up Yet?
-        <Link href="#">Sign up</Link>
-      </SignInHeader>
-      <Label>Email</Label>
-      <Input type="email" placeholder="styled@codeit.kr" />
-      <Label>Password</Label>
-      <Input type="password" placeholder="password" />
-      <Button>Sign in</Button>
-      <KakaoButton>Sign in for Kakao</KakaoButton>
-    </Container>
+    <>
+      <GlobalStyle />
+      <Container>
+        <Header>
+          <CodeitLogo src={CodeitLogoImg} alt="Codeit" />
+          Have you Signed up Yet?
+          <Link href="#">Sign up</Link>
+        </Header>
+        <Label>Email</Label>
+        <Input type="email" placeholder="styled@codeit.kr" />
+        <Label>Password</Label>
+        <Input type="password" placeholder="password" />
+        <Button>Sign in</Button>
+        <KakaoButton>Sign in for Kakao</KakaoButton>
+      </Container>
+    </>
   );
 }
 
